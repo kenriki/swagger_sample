f869fe013982ddd12b9afdc28a66c0792e038dea
kenriki <riki.kenmochi@gmail.com>
2021-08-23 21:55:30 +0900

    インデント修正

--- src/main/resources/static/js/top.js
+++ src/main/resources/static/js/top.js
@@ -3,13 +3,13 @@ var app = app || {
 		var html = "";
 
 		// 一桁の数字を0埋め
-	    var fm = function(num) {
-	        num += "";
-	        if (num.length == 1) {
-	            num = "0" + num;
-	        }
-	        return num;
-	    };
+		var fm = function(num) {
+			num += "";
+			if (num.length == 1) {
+				num = "0" + num;
+			}
+			return num;
+		};
 
 		var date = new Date();
 
@@ -18,14 +18,15 @@ var app = app || {
 		var hours = fm(date.getHours());
 
 		var year = fm(date.getFullYear());
-		var month = fm(date.getMonth() + 1) ;
+		var month = fm(date.getMonth() + 1);
 
 		var day = fm(date.getDate());
 
-		html = year + "/" + month + "/" + day + "::" + hours + ":" + minutes + ":" + seconds;
+		html = year + "/" + month + "/" + day + "::" + hours + ":"
+				+ minutes + ":" + seconds;
 		document.getElementById("id-result").innerHTML = html;
 
-		setInterval("app.dispDate()",1000);
+		setInterval("app.dispDate()", 1000);
 	}
 };
 
