1474ff527aa3646d5b392412aadac3c0d6f50578
kenriki <riki.kenmochi@gmail.com>
2021-09-09 22:14:08 +0900

    #2 springdocのopenapi3 試す

--- src/main/java/com/example/homeweb/SwaggerConfig.java
+++ src/main/java/com/example/homeweb/SwaggerConfig.java
@@ -1,75 +1,47 @@
 package com.example.homeweb;
 
-import java.util.ArrayList;
-
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
-
-import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.DocExpansion;
-import springfox.documentation.swagger.web.ModelRendering;
-import springfox.documentation.swagger.web.OperationsSorter;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
-@EnableOpenApi
 public class SwaggerConfig {
 
-    @Bean
-    public Docket swaggerSpringMvcPlugin() {
-        return new Docket(DocumentationType.OAS_30)
-                .groupName("sample-api")    // APIドキュメントをグルーピングするための識別名
-                .select()
-                .paths(paths())
-                .build()
-                .apiInfo(apiInfo());
-    }
+	@Bean
+	public GroupedOpenApi publicApi() {
+		return GroupedOpenApi.builder()
+				.group("springshop-public")
+				.pathsToMatch("/public/**")
+				.build();
+	}
 
-    private Predicate<String> paths() {
-        // ドキュメント生成の対象とするAPIのURLを指定
-        // この場合、「/user」で始まるAPIがドキュメント生成対象となる
-        return Predicates.or(
-        		Predicates.containsPattern("/*"),
-        		Predicates.containsPattern("/user")
-        		);
-    }
+	@Bean
+	public GroupedOpenApi adminApi() {
+		return GroupedOpenApi.builder()
+				.group("springshop-admin")
+				.pathsToMatch("/user/**")
+				.build();
+	}
 
-    private ApiInfo apiInfo() {
-        // http://springfox.github.io/springfox/javadoc/2.6.0/index.html?springfox/documentation/service/ApiInfo.html
-        return new ApiInfo(
-                  "Sample API"  // APIのタイトル
-                , "このAPIはサンプルです"  // APIの説明
-                , "V1"  // APIのバージョン
-                , "????"    // よくわからない
-                , new Contact(
-                         "株式会社XXXXXXX"      // APIに関する連絡先組織・団体等
-                        ,"http://XXXXXXXXXXX.co.jp" // APIに関する連絡先組織・団体等のWeb Site Url
-                        ,"XXXXXXXX@example.jp")     // APIに関する連絡先組織・団体等のメールアドレス
-                , "API LICENSE" // APIのライセンス
-                , "http://XXXXXXXXXXXX.co.jp"   // APIのライセンスURL
-                , new ArrayList<VendorExtension>()  // 独自に拡張したいドキュメントがあればここで作成
-        );
-    }
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI().components(new Components()
+				.addSecuritySchemes("basicScheme", new SecurityScheme()
+				.type(SecurityScheme.Type.HTTP).scheme("basic")))
+				.info(new Info().title("SpringShop API")
+						.description("Spring shop sample application")
+						.version("v0.0.1")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
 
-    @Bean
-    public UiConfiguration swaggerUiConf() {
-        return UiConfigurationBuilder.builder()
-                .displayRequestDuration(true)
-                .defaultModelExpandDepth(-1)
-                .defaultModelRendering(ModelRendering.EXAMPLE)
-                .filter(true)
-                .operationsSorter(OperationsSorter.ALPHA)
-                .validatorUrl("")
-                .docExpansion(DocExpansion.LIST)
-                .build();
-    }
+				.externalDocs(new ExternalDocumentation()
+						.description("SpringShop Wiki Documentation")
+						.url("https://springshop.wiki.github.org/docs"));
+	}
 }
